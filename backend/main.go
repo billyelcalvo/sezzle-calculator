72af795015a20864808120d2a4119fe2e97b97ec
billyelcalvo <dam0806blas@gmail.com>
2026-09-11 16:32:22 -0500

    feat: wire calculator HTTP server

--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/diegomora/sezzle-calculator/internal/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/api/calculate", handlers.Calculate)
+
+	server := &http.Server{
+		Addr:              ":8080",
+		Handler:           mux,
+		ReadHeaderTimeout: 5 * time.Second,
+	}
+
+	log.Printf("calculator API listening on %s", server.Addr)
+	log.Fatal(server.ListenAndServe())
+}
